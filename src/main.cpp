7e4ee767debb3f1f30a3aac92b8760955abaec7f
Spencer Johns <s.johns3404@gmail.com>
2025-03-26 12:14:29 -0600

    Added loop to main that checks random byte generator and the associated integer value.

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,12 @@
 int main(int argc, char** argv) {
 	CHIP8 chip;
 
-	uint8_t val = static_cast<uint8_t>(chip.randByte(chip.randGen));
+	// Loop checking random character generation
+	for (int i = 0; i < 15; i++) {
+		uint8_t val = static_cast<uint8_t>(chip.randByte(chip.randGen));
 
-	cout << val << " " << int(val) << endl;
+		cout << val << ":     " << int(val) << endl;
+	}
 
 	return 0;
 }
